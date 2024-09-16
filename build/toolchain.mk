8994215e59e4deeeaf60c8c30eb9fd3c5366b75c
liuqi <liuqi@bupt.edu.cn>
2024-09-16 21:44:33 +0800

    fix: change gnu-toolchain from 12 to 11

--- build/toolchain.mk
+++ build/toolchain.mk
@@ -4,6 +4,7 @@
 SHELL				= /bin/bash
 ROOT				?= $(CURDIR)/..
 TOOLCHAIN_ROOT 			?= $(ROOT)/toolchains
+RUST_TOOLCHAIN_PATH 		?= $(TOOLCHAIN_ROOT)/rust
 UNAME_M				:= $(shell uname -m)
 ARCH				?= arm
 
@@ -44,23 +45,33 @@ define build_toolchain
 	@touch $2/.done
 endef
 
+# Download the Rust toolchain
+define dl-rust-toolchain
+	@if [ ! -d "$(1)" ]; then \
+		mkdir -p $(1) && \
+		export RUSTUP_HOME=$(1)/.rustup && \
+		export CARGO_HOME=$(1)/.cargo && \
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path; \
+	fi
+endef
+
 ifeq ($(UNAME_M),x86_64)
 ifeq ($(ARCH),arm)
 # Please keep in sync with br-ext/configs/toolchain-aarch32
 # and below for aarch64 host
 AARCH32_PATH 			?= $(TOOLCHAIN_ROOT)/aarch32
 AARCH32_CROSS_COMPILE 		?= $(AARCH32_PATH)/bin/arm-linux-gnueabihf-
-AARCH32_GCC_VERSION 		?= arm-gnu-toolchain-12.2.rel1-x86_64-arm-none-linux-gnueabihf
-SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/$(AARCH32_GCC_VERSION).tar.xz
+AARCH32_GCC_VERSION 		?= arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-linux-gnueabihf
+SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/$(AARCH32_GCC_VERSION).tar.xz
 
 # Please keep in sync with br-ext/configs/toolchain-aarch64
 AARCH64_PATH 			?= $(TOOLCHAIN_ROOT)/aarch64
 AARCH64_CROSS_COMPILE 		?= $(AARCH64_PATH)/bin/aarch64-linux-gnu-
-AARCH64_GCC_VERSION 		?= arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-linux-gnu
-SRC_AARCH64_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/$(AARCH64_GCC_VERSION).tar.xz
+AARCH64_GCC_VERSION 		?= arm-gnu-toolchain-11.3.rel1-x86_64-aarch64-none-linux-gnu
+SRC_AARCH64_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/$(AARCH64_GCC_VERSION).tar.xz
 
 .PHONY: toolchains
-toolchains: aarch32-toolchain aarch64-toolchain
+toolchains: aarch32-toolchain aarch64-toolchain rust-toolchain
 
 .PHONY: aarch32-toolchain
 aarch32-toolchain:
@@ -70,8 +81,40 @@ aarch32-toolchain:
 aarch64-toolchain:
 	$(call dltc,$(AARCH64_PATH),$(SRC_AARCH64_GCC),$(AARCH64_GCC_VERSION))
 
+.PHONY: rust-toolchain
+rust-toolchain:
+	$(call dl-rust-toolchain,$(RUST_TOOLCHAIN_PATH))
+
 CLANG_VER			?= 12.0.0
 CLANG_PATH			?= $(ROOT)/clang-$(CLANG_VER)
+
+# Download the Clang compiler with LLVM tools and compiler-rt libraries
+define dl-clang
+	@if [ ! -d "$(2)" ]; then \
+		./get_clang.sh $(1) $(2); \
+	else \
+		echo "$(2) already exists"; \
+	fi
+endef
+
+.PHONY: clang-toolchains
+clang-toolchains:
+	$(call dl-clang,$(CLANG_VER),$(CLANG_PATH))
+
+else ifeq ($(ARCH),riscv)
+RISCV64_PATH 			?= $(TOOLCHAIN_ROOT)/riscv64
+RISCV64_CROSS_COMPILE 		?= $(RISCV64_PATH)/bin/riscv64-unknown-linux-gnu-
+RISCV64_GCC_RELEASE_DATE	?= 2023.07.07
+RISCV64_GCC_VERSION		?= riscv64-glibc-ubuntu-22.04-gcc-nightly-$(RISCV64_GCC_RELEASE_DATE)-nightly
+SRC_RISCV64_GCC			?= https://github.com/riscv-collab/riscv-gnu-toolchain/releases/download/$(RISCV64_GCC_RELEASE_DATE)/$(RISCV64_GCC_VERSION).tar.gz
+
+.PHONY: toolchains
+toolchains: riscv64-toolchain
+
+.PHONY: riscv64-toolchain
+riscv64-toolchain:
+	$(call dltc,$(RISCV64_PATH),$(SRC_RISCV64_GCC),$(RISCV64_GCC_VERSION))
+
 endif
 
 else ifeq ($(UNAME_M),aarch64)
@@ -80,8 +123,8 @@ else ifeq ($(UNAME_M),aarch64)
 # and above for x86_64 host
 AARCH32_PATH 			?= $(TOOLCHAIN_ROOT)/aarch32
 AARCH32_CROSS_COMPILE 		?= $(AARCH32_PATH)/bin/arm-linux-gnueabihf-
-AARCH32_GCC_VERSION 		?= arm-gnu-toolchain-12.2.rel1-aarch64-arm-none-linux-gnueabihf
-SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/12.2.rel1/binrel/$(AARCH32_GCC_VERSION).tar.xz
+AARCH32_GCC_VERSION 		?= arm-gnu-toolchain-11.3.rel1-aarch64-arm-none-linux-gnueabihf
+SRC_AARCH32_GCC 		?= https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/$(AARCH32_GCC_VERSION).tar.xz
 
 # There isn't any native aarch64 toolchain released from Arm and buildroot
 # doesn't support distribution toolchain [1]. So we are left with no choice
@@ -92,7 +135,7 @@ AARCH64_PATH 			?= $(TOOLCHAIN_ROOT)/aarch64
 AARCH64_CROSS_COMPILE 		?= $(AARCH64_PATH)/bin/aarch64-linux-
 
 .PHONY: toolchains
-toolchains: aarch32-toolchain $(AARCH64_PATH)/.done
+toolchains: aarch32-toolchain $(AARCH64_PATH)/.done rust-toolchain
 
 .PHONY: aarch32-toolchain
 aarch32-toolchain:
@@ -100,6 +143,10 @@ aarch32-toolchain:
 
 $(AARCH64_PATH)/.done:
 	$(call build_toolchain,aarch64,$(AARCH64_PATH),aarch64,gnu)
+
+.PHONY: rust-toolchain
+rust-toolchain:
+	$(call dl-rust-toolchain,$(RUST_TOOLCHAIN_PATH))
 
 else # $(UNAME_M) != x86_64 or $(UNAME_M) != aarch64
 AARCH32_PATH 			:= $(TOOLCHAIN_ROOT)/aarch32
@@ -116,3 +163,16 @@ $(AARCH64_PATH)/.done:
 $(AARCH32_PATH)/.done:
 	$(call build_toolchain,aarch32,$(AARCH32_PATH),arm,gnueabihf)
 endif
+
+# Recipe to build Clang from sources and install it in
+# $(TOOLCHAIN_ROOT)/clang-x.y.z/bin
+
+CLANG_BUILD_VER_MAJ=18
+CLANG_BUILD_VER=$(CLANG_BUILD_VER_MAJ).1.7
+CLANG_BUILD_IMAGE=clang-$(CLANG_BUILD_VER)-$(UNAME_M)
+
+clang-toolchains-build:
+	docker build --build-arg VER_MAJ=$(CLANG_BUILD_VER_MAJ) --build-arg VER=$(CLANG_BUILD_VER) -t $(CLANG_BUILD_IMAGE) clang/
+	id=$$(docker create $(CLANG_BUILD_IMAGE)) && \
+		docker cp $${id}:/root/clang-$(CLANG_BUILD_VER) $(TOOLCHAIN_ROOT) && \
+		docker rm $${id}
--- init-repo.sh
+++
@@ -1,7 +0,0 @@
-git clone --depth=1 -b  4.3.0  https://github.com/OP-TEE/optee_test.git
-git clone --depth=1 -b  4.3.0  https://github.com/OP-TEE/optee_os.git
-git clone --depth=1 -b  4.3.0  https://github.com/OP-TEE/optee_client.git
-git clone --depth=1 -b  4.3.0  https://github.com/linaro-swg/optee_examples.git
-git clone --depth=1 -b  2024.5  https://github.com/buildroot/buildroot
-git clone --depth=1 -b  xlnx_rebase_v2.8  https://github.com/Xilinx/arm-trusted-firmware
-git clone --depth=1 -b  xilinx_v2021.1 https://github.com/Xilinx/bootgen
