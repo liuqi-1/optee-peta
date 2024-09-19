8ac26ed0d236aa223ef6342cf7bb25c2a2a06057
liuqi <liuqi@bupt.edu.cn>
2024-09-19 12:15:47 +0800

    add bootgen to rootfs

--- build/install.sh
+++ build/install.sh
@@ -3,8 +3,8 @@ sudo rm -rf /media/liuqi/ROOT/*
 ls /media/liuqi/ROOT
 ls /media/liuqi/BOOT
 echo "finish clearing previous image"
-cp images/BOOT.BIN /media/liuqi/BOOT
-cp images/boot.scr /media/liuqi/BOOT
-cp images/image.ub /media/liuqi/BOOT
+cp images/BOOT.BIN /media/liuqi/BOOT/
+cp images/boot.scr /media/liuqi/BOOT/
+cp images/image.ub /media/liuqi/BOOT/
 sudo tar xvfp images/rootfs.tar.gz -C /media/liuqi/ROOT
 echo "finish installing new boot image"
